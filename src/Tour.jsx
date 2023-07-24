530fbb3ba6f805d9ddec4ee7d460e2dbd7c5338a
Mokshparashar <parasharm239@gmail.com>
2023-07-24 07:04:16 +0530

    Update Tour.jsx

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import { useGlobalContext } from "./context";
+import { useState } from "react";
 
 const Tour = ({ id, name, image, price, info }) => {
   const { deleteTour, stringModification, ...state } = useGlobalContext();
-  return (
-    <div key={id} className="single-tour">
-      <img src={image} alt="" className="img" />
-      <div className="tour-info">
-        <h5>{name}</h5>
-        <p>
-          {state.showMore ? info.substring(1, 150) : info}
-          <button className="info-btn" onClick={() => stringModification(id)}>
-            {state.showMore ? "...show more" : "show less"}
-          </button>
-        </p>
+  const [showMore, setShowMore] = useState(true);
+
+  if (state.tours.size === 0) {
+    return <h1>No tours</h1>;
+  } else {
+    return (
+      <div key={id} className="single-tour">
+        <img src={image} alt="" className="img" />
+        <div className="tour-info">
+          <h5>{name}</h5>
+          <p>
+            {showMore ? info.substring(1, 150) : info}
+            <button className="info-btn" onClick={() => setShowMore(!showMore)}>
+              {showMore ? "...show more" : "show less"}
+            </button>
+          </p>
+        </div>
+        <div className="tour-price">${price}</div>
+        <button className="delete-btn btn" onClick={() => deleteTour(id)}>
+          Not interested
+        </button>
       </div>
-      <div className="tour-price">${price}</div>
-      <button className="delete-btn btn" onClick={() => deleteTour(id)}>
-        Not interested
-      </button>
-    </div>
-  );
+    );
+  }
 };
 
 export default Tour;
